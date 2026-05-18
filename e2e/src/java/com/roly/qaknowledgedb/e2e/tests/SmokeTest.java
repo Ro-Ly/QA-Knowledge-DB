8f90128620ac1694d4498e7d2a8d2d59e8c2944c
ruslanlyzko <ro.lyzko@gmail.com>
2026-05-18 22:35:01 +0100

    - allure reporting is added - allure is added into the circle CI

--- e2e/src/java/com/roly/qaknowledgedb/e2e/tests/SmokeTest.java
+++ e2e/src/java/com/roly/qaknowledgedb/e2e/tests/SmokeTest.java
@@ -2,10 +2,16 @@ package com.roly.qaknowledgedb.e2e.tests;
 
 import com.roly.qaknowledgedb.e2e.BaseTest;
 import com.roly.qaknowledgedb.e2e.pages.KnowledgeBasePage;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.junit.jupiter.api.Test;
 
 class SmokeTest extends BaseTest {
 
+    @Epic("Frontend")
+    @Story("Base smoke test")
+    @Feature("Section title basic check")
     @Test
     void shouldOpenKnowledgeBaseAndShowQuestions() {
         new KnowledgeBasePage(page)
